da7ea42e63f0a3827230002b5a94d86399f1c5e0
Renato Teles Pereira Filho <renatotelesa13@gmail.com>
2025-09-23 00:55:36 -0300

    Add files via upload

---
+++ superheroes/-- Consultas SQL.sql
@@ -0,0 +1,30 @@
+-- Consultas SQL
+
+--Primeira 
+SELECT * 
+FROM superhero;
+
+--Segunda 
+SELECT superhero_name, publisher_id
+FROM superhero;
+
+--Terceira
+SELECT id, superhero_name, publisher_id
+FROM superhero
+WHERE publisher_id = 1;
+
+--Quarta 
+SELECT *
+FROM superhero
+WHERE superhero_name LIKE 'S%';
+
+--Quinta
+SELECT id, superhero_name, publisher_id
+FROM superhero
+WHERE publisher_id IN (1, 2, 3);
+
+--Sexta
+SELECT *
+FROM superhero
+WHERE publisher_id = 1
+OR superhero_name LIKE 'B%';
